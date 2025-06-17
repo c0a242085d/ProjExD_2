84dcae54b13866c983eda1ac9e79af1791bfa595
Tenta Hiratsuka <c0a242085d@edu.teu.ac.jp>
2025-06-17 14:37:05 +0900

    練習３終了

--- dodge_bomb.py
+++ dodge_bomb.py
@@ -12,6 +12,19 @@ DELTA = { # 移動量辞書
     pg.K_RIGHT: (+5, 0),
 }
 os.chdir(os.path.dirname(os.path.abspath(__file__)))
+    
+def check_bound(rct: pg.Rect) -> tuple[bool, bool]:
+    """
+    引数：こうかとんRectか爆弾Rect
+    戻り値：横方向、縦方向の画面内外判定結果
+    画面内ならTrue,画面外ならFalse
+    """
+    yoko, tate = True , True #初期値 画面内
+    if rct.left < 0 or WIDTH < rct.right:
+        yoko = False
+    if rct.top < 0 or HEIGHT < rct.bottom:
+        tate = False
+    return yoko, tate
 
 
 def main():
@@ -51,9 +64,16 @@ def main():
         #if key_lst[pg.K_RIGHT]:
             #sum_mv[0] += 5
         kk_rct.move_ip(sum_mv)
+        if check_bound(kk_rct) !=(True,True):
+            kk_rct.move_ip(-sum_mv[0],-sum_mv[1])#移動がなかったことにする
         screen.blit(kk_img, kk_rct)
-        bb_rct.move_ip(vx,vy)
-        screen.blit(bb_img, bb_rct)
+        bb_rct.move_ip(vx,vy) #爆弾の移動
+        yoko, tate = check_bound(bb_rct)
+        if not yoko: #
+            vx *= -1
+        if not tate:
+            vy *= -1
+        screen.blit(bb_img, bb_rct) #
         pg.display.update()
         tmr += 1
         clock.tick(50)

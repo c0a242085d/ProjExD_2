4f4597a24943e65c07d07f296eb692985971f295
Tenta Hiratsuka <c0a242085d@edu.teu.ac.jp>
2025-06-17 16:49:53 +0900

    演習課題２終了

--- dodge_bomb.py
+++ dodge_bomb.py
@@ -44,6 +44,19 @@ def gameover(screen: pg.Surface) -> None:
     screen.blit(text, text_rect)
     pg.display.update()
     time.sleep(5)  
+
+def init_bb_imgs() -> tuple[list[pg.Surface], list[int]]:
+    """
+    爆弾の画像リストと加速度リストを返す
+    """
+    bb_imgs = []
+    bb_accs = [a for a in range(1, 11)]
+    for r in range(1, 11):
+        bb_img = pg.Surface((20*r, 20*r))
+        pg.draw.circle(bb_img, (255, 0, 0), (10*r, 10*r), 10*r)
+        bb_img.set_colorkey((0, 0, 0))
+        bb_imgs.append(bb_img)
+    return bb_imgs, bb_accs
     
 
 def main():
@@ -62,13 +75,21 @@ def main():
     vx, vy = +5, +5
     clock = pg.time.Clock()
     tmr = 0
+
+    bb_imgs, bb_accs = init_bb_imgs()
+
+    vx, vy = +5, +5  # 最初の速度
+    tmr = 0          # 経過時間カウンタ
+
     while True:
         for event in pg.event.get():
             if event.type == pg.QUIT: 
-                return
-        if kk_rct.colliderect(bb_rct): #こうかとん
+             return
+
+        if kk_rct.colliderect(bb_rct):  # こうかとんと爆弾が衝突
             gameover(screen)
             return
+
         screen.blit(bg_img, [0, 0]) 
 
         key_lst = pg.key.get_pressed()
@@ -76,29 +97,36 @@ def main():
         for key, mv in DELTA.items():
             if key_lst[key]:
                 sum_mv[0] += mv[0]
-                sum_mv[1] += mv[1]    
-        #if key_lst[pg.K_UP]:
-           # sum_mv[1] -= 5
-        #if key_lst[pg.K_DOWN]:
-           # sum_mv[1] += 5
-        #if key_lst[pg.K_LEFT]:
-            #sum_mv[0] -= 5
-        #if key_lst[pg.K_RIGHT]:
-            #sum_mv[0] += 5
+                sum_mv[1] += mv[1]
+
         kk_rct.move_ip(sum_mv)
-        if check_bound(kk_rct) !=(True,True):
-            kk_rct.move_ip(-sum_mv[0],-sum_mv[1])#移動がなかったことにする
+        if check_bound(kk_rct) != (True, True):
+            kk_rct.move_ip(-sum_mv[0], -sum_mv[1])  # 元に戻す
+
         screen.blit(kk_img, kk_rct)
-        bb_rct.move_ip(vx,vy) #爆弾の移動
+
+        idx = min(tmr // 500, 9)
+        bb_img = bb_imgs[idx]
+        acc = bb_accs[idx]    
+        bb_imgs, bb_accs = init_bb_imgs()
+        avx = vx*bb_accs[min(tmr//500, 9)]
+        bb_img = bb_imgs[min(tmr//500, 9)]
+
+
+        bb_rct.move_ip(vx * acc, vy * acc)
+    
         yoko, tate = check_bound(bb_rct)
         if not yoko: 
             vx *= -1
         if not tate:
             vy *= -1
-        screen.blit(bb_img, bb_rct) 
+
+        screen.blit(bb_img, bb_rct)
+
         pg.display.update()
         tmr += 1
         clock.tick(50)
+
 
 
 if __name__ == "__main__":
